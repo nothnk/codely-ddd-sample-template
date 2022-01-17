f1bfdbbba2cd12fcc42e65a687fc34b0bdcd582d
Queli Coto <quelicm@gmail.com>
2022-01-17 21:17:57 +0100

    [main]: ♻️ Add CourseId value-object

--- src/Contexts/Mooc/Courses/domain/Course.ts
+++ src/Contexts/Mooc/Courses/domain/Course.ts
@@ -1,11 +1,11 @@
-import { Uuid } from '../../../Shared/domain/value-object/Uuid';
+import { CourseId } from '../../Shared/domain/CourseId';
 
 export class Course {
-  readonly id: Uuid;
+  readonly id: CourseId;
   readonly name: string;
   readonly duration: string;
 
-  constructor({ id, name, duration }: { id: Uuid; name: string; duration: string; }) {
+  constructor({ id, name, duration }: { id: CourseId; name: string; duration: string; }) {
     this.id = id;
     this.name = name;
     this.duration = duration;

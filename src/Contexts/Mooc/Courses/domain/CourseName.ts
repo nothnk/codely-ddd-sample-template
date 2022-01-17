d9f04d1ded98679be37cc7f7c5b957d15a3e64bf
Queli Coto <quelicm@gmail.com>
2022-01-17 21:36:10 +0100

    [main]: ✨ Add CourseName value object

---
+++ src/Contexts/Mooc/Courses/domain/CourseName.ts
@@ -0,0 +1,16 @@
+import { StringValueObject } from "../../../Shared/domain/value-object/StringValueObject";
+import { CourseNameLengthExceeded } from "./CourseNameLengthExceeded";
+
+export class CourseName extends StringValueObject {
+  constructor(value: string) {
+    super(value);
+    this.ensureLengthIsLessThan30Characters(value);
+  }
+
+  private ensureLengthIsLessThan30Characters(value: string): void {
+    if (value.length > 30 ) {
+      throw new CourseNameLengthExceeded(`The course name <${value}> has more than 30 characteres`);
+    }
+  }
+
+}

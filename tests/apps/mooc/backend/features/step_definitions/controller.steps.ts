9f36edda5c1181b3268a6ac1249f6c6956b82dab
Queli Coto <quelicm@gmail.com>
2022-01-22 13:07:39 +0100

    [main]: 🧪 Fix github & close db connection

--- tests/apps/mooc/backend/features/step_definitions/controller.steps.ts
+++ tests/apps/mooc/backend/features/step_definitions/controller.steps.ts
@@ -2,6 +2,8 @@ import assert from 'assert';
 import { AfterAll, BeforeAll, Given, Then } from 'cucumber';
 import request from 'supertest';
 import { MoocBackendApp } from '../../../../../../src/apps/mooc/backend/MoocBackendApp';
+import container from '../../../../../../src/apps/mooc/backend/dependency-injection';
+import { EnvironmentArranger } from '../../../../../Contexts/Shared/infrastructure/arranger/EnvironmentArranger';
 
 let _request: request.Test;
 let application: MoocBackendApp;
@@ -24,10 +26,14 @@ Then('the response should be empty', () => {
 });
 
 BeforeAll(async () => {
+  const environmentArranger: Promise<EnvironmentArranger> = container.get('Mooc.EnvironmentArranger');
+  await (await environmentArranger).arrange();
   application = new MoocBackendApp();
   await application.start();
 });
 
 AfterAll(async () => {
+  const environmentArranger: Promise<EnvironmentArranger> = container.get('Mooc.EnvironmentArranger');
+  await (await environmentArranger).close();
   await application.stop();
 });
